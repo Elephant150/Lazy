e99dfafaec93529e0cd513347af175d2c9e745a9
Elephant150 <dmytruk150@gmail.com>
2019-05-07 10:27:51 +0300

    Main page+design+adaptation

---
+++ header.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang=en>
+<head>
+    <meta charset=utf-8>
+    <meta name=viewport content='' width=device-width, initial-scale=1.0>
+    <!--    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">-->
+    <!--    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"-->
+    <!--          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">-->
+    <!--    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>-->
+    <!--    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>-->
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Anaheim" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+          integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Lazy</title>
+</head>
+<body>
+<div id="wrapper">
+
+    <!-- Sidebar -->
+    <div id="sidebar-wrapper">
+        <ul class="sidebar-nav">
+            <li class="sidebar-brand"><a href="/"><img src="img/white-logo.png" alt=""></a></li>
+            <li><a href=/>Main</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Message</a></li>
+            <li><a href="#">Friends</a></li>
+            <li><a href="#">Music</a></li>
+            <li><a href="#">Portfolio</a></li>
+            <li><a href="#">ToDo List</a></li>
+            <li><a href="#">Calendar</a></li>
+            <li><a href="#">Notebook</a></li>
+            <li><a href="#">Training</a></li>
+            <li><a href="#">Support</a></li>
+        </ul>
+    </div>
+
+
+    <!--       header-->
+    <!--    <nav class="top-bar navbar-top navbar-expand-lg navbar-light">-->
+    <!--        <a href="#menu-toggle" id="menu-toggle"><i class="fas fa-angle-double-right"></i></a>-->
+    <!--        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"-->
+    <!--                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">-->
+    <!--            <i class="fas fa-bars"></i>-->
+    <!--        </button>-->
+    <!--        <div class="collapse navbar-collapse" id="navbarNavDropdown">-->
+    <!--            <ul class="navbar-nav">-->
+    <!--                <li class="nav-item ">-->
+    <!--                    <a class="nav-link" href="#">Home </span></a>-->
+    <!--                </li>-->
+    <!--                <li class="nav-item">-->
+    <!--                    <a class="nav-link" href="#">Features</a>-->
+    <!--                </li>-->
+    <!--                <li class="nav-item">-->
+    <!--                    <a class="nav-link" href="#">Pricing</a>-->
+    <!--                </li>-->
+    <!---->
+    <!--            </ul>-->
+    <!--        </div>-->
+    <!--    </nav>-->
+
+
+    <nav class="top-bar navbar navbar-default navbar-fixed-top">
+        <div class="container">
+
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse"
+                        data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a href="#menu-toggle" id="menu-toggle"><i class="btn-sidebar fas fa-angle-double-right"></i></a><a
+                        class="navbar-brand" href="/"><img src="img/white-logo.png" alt=""></a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#portfolio">Player</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#about">About</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="login.php">Sing in</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav
---
+++ index.php
@@ -0,0 +1,135 @@
+<?php include 'header.php' ?>
+
+    <div id="page-content-wrapper">
+        <section>
+        <div class="container-fluid">
+            <!--        <div class="container">-->
+            <div class="row">
+                <div class="col-lg-10">
+                    <h1>Some content</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur consequuntur doloribus
+                        earum,
+                        eum iusto laudantium maiores mollitia, non quibusdam, sed tempora veniam? Aliquid cumque,
+                        dignissimos nesciunt nihil odit perferendis quod?</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci consequatur cumque ea eius
+                        impedit
+                        labore laboriosam, laborum, magnam officiis provident quaerat quidem quod repellat repellendus
+                        sequi
+                        similique sit tempora voluptate..</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur consequuntur doloribus
+                        earum,
+                        eum iusto laudantium maiores mollitia, non quibusdam, sed tempora veniam? Aliquid cumque,
+                        dignissimos nesciunt nihil odit perferendis quod?</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci consequatur cumque ea eius
+                        impedit
+                        labore laboriosam, laborum, magnam officiis provident quaerat quidem quod repellat repellendus
+                        sequi
+                        similique sit tempora voluptate..</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur consequuntur doloribus
+                        earum,
+                        eum iusto laudantium maiores mollitia, non quibusdam, sed tempora veniam? Aliquid cumque,
+                        dignissimos nesciunt nihil odit perferendis quod?</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci consequatur cumque ea eius
+                        impedit
+                        labore laboriosam, laborum, magnam officiis provident quaerat quidem quod repellat repellendus
+                        sequi
+                        similique sit tempora voluptate..</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur consequuntur doloribus
+                        earum,
+                        eum iusto laudantium maiores mollitia, non quibusdam, sed tempora veniam? Aliquid cumque,
+                        dignissimos nesciunt nihil odit perferendis quod?</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci consequatur cumque ea eius
+                        impedit
+                        labore laboriosam, laborum, magnam officiis provident quaerat quidem quod repellat repellendus
+                        sequi
+                        similique sit tempora voluptate..</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur consequuntur doloribus
+                        earum,
+                        eum iusto laudantium maiores mollitia, non quibusdam, sed tempora veniam? Aliquid cumque,
+                        dignissimos nesciunt nihil odit perferendis quod?</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci consequatur cumque ea eius
+                        impedit
+                        labore laboriosam, laborum, magnam officiis provident quaerat quidem quod repellat repellendus
+                        sequi
+                        similique sit tempora voluptate..</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur consequuntur doloribus
+                        earum,
+                        eum iusto laudantium maiores mollitia, non quibusdam, sed tempora veniam? Aliquid cumque,
+                        dignissimos nesciunt nihil odit perferendis quod?</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci consequatur cumque ea eius
+                        impedit
+                        labore laboriosam, laborum, magnam officiis provident quaerat quidem quod repellat repellendus
+                        sequi
+                        similique sit tempora voluptate..</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur consequuntur doloribus
+                        earum,
+                        eum iusto laudantium maiores mollitia, non quibusdam, sed tempora veniam? Aliquid cumque,
+                        dignissimos nesciunt nihil odit perferendis quod?</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci consequatur cumque ea eius
+                        impedit
+                        labore laboriosam, laborum, magnam officiis provident quaerat quidem quod repellat repellendus
+                        sequi
+                        similique sit tempora voluptate..</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur consequuntur doloribus
+                        earum,
+                        eum iusto laudantium maiores mollitia, non quibusdam, sed tempora veniam? Aliquid cumque,
+                        dignissimos nesciunt nihil odit perferendis quod?</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci consequatur cumque ea eius
+                        impedit
+                        labore laboriosam, laborum, magnam officiis provident quaerat quidem quod repellat repellendus
+                        sequi
+                        similique sit tempora voluptate..</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur consequuntur doloribus
+                        earum,
+                        eum iusto laudantium maiores mollitia, non quibusdam, sed tempora veniam? Aliquid cumque,
+                        dignissimos nesciunt nihil odit perferendis quod?</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci consequatur cumque ea eius
+                        impedit
+                        labore laboriosam, laborum, magnam officiis provident quaerat quidem quod repellat repellendus
+                        sequi
+                        similique sit tempora voluptate..</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur consequuntur doloribus
+                        earum,
+                        eum iusto laudantium maiores mollitia, non quibusdam, sed tempora veniam? Aliquid cumque,
+                        dignissimos nesciunt nihil odit perferendis quod?</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci consequatur cumque ea eius
+                        impedit
+                        labore laboriosam, laborum, magnam officiis provident quaerat quidem quod repellat repellendus
+                        sequi
+                        similique sit tempora voluptate..</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur consequuntur doloribus
+                        earum,
+                        eum iusto laudantium maiores mollitia, non quibusdam, sed tempora veniam? Aliquid cumque,
+                        dignissimos nesciunt nihil odit perferendis quod?</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci consequatur cumque ea eius
+                        impedit
+                        labore laboriosam, laborum, magnam officiis provident quaerat quidem quod repellat repellendus
+                        sequi
+                        similique sit tempora voluptate..</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur consequuntur doloribus
+                        earum,
+                        eum iusto laudantium maiores mollitia, non quibusdam, sed tempora veniam? Aliquid cumque,
+                        dignissimos nesciunt nihil odit perferendis quod?</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci consequatur cumque ea eius
+                        impedit
+                        labore laboriosam, laborum, magnam officiis provident quaerat quidem quod repellat repellendus
+                        sequi
+                        similique sit tempora voluptate..</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur consequuntur doloribus
+                        earum,
+                        eum iusto laudantium maiores mollitia, non quibusdam, sed tempora veniam? Aliquid cumque,
+                        dignissimos nesciunt nihil odit perferendis quod?</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci consequatur cumque ea eius
+                        impedit
+                        labore laboriosam, laborum, magnam officiis provident quaerat quidem quod repellat repellendus
+                        sequi
+                        similique sit tempora voluptate..</p>
+
+                </div>
+            </div>
+            <!--        </div>-->
+        </div>
+            <?php include 'footer.php' ?>
+        </section>
+    </div>
+
